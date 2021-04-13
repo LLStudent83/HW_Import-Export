e925b40fd29a13dc75cbec63005ad786ecd00dcc
Dmitry Prigulnov <prigulnov@mail.ru>
2021-04-13 21:26:35 +0500

    13/04 End

---
+++ src/js/game.js
@@ -0,0 +1,17 @@
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
+
+  import Character from `./domain.js`;
+  export {GameSavingData, readGameSaving, writeGameSaving};
