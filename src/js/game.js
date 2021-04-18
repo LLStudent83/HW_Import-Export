34daced62cc2900c6fed788b290efbab5de90eac
Dmitry Prigulnov <prigulnov@mail.ru>
2021-04-18 11:54:35 +0500

    Исправил /

--- src/js/game.js
+++ src/js/game.js
@@ -1,17 +1,14 @@
 export default class Game {
-    start() {
-      console.log('game started');
-    }
+  start() {
+    console.log("game started");
   }
-  
-  class GameSavingData {
-  }
-  
-  function readGameSaving() {
-  }
-  
-  function writeGameSaving() {
-  }
+}
 
-  import Character from './domain.js';
-  export {GameSavingData, readGameSaving, writeGameSaving};
+class GameSavingData {}
+
+function readGameSaving() {}
+
+function writeGameSaving() {}
+
+import Character from './domain.js';
+export { GameSavingData, readGameSaving, writeGameSaving };
